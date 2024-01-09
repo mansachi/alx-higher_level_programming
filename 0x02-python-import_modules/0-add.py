32507f8d1ba252dde2e42a7ddbd728ea9d8dd307
mansachi <amadichinda1001@outlook.com>
2024-01-09 22:16:15 +0100

    0x02-Python-Import-Modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# code for 0x02-python-import_modules
+
+if __name__ == "__main__":
+    """Print the sum of one and two."""
+    from add_0 importb add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
