0b9bbfd32f0772090389bd224f7d8b4f74e3321a
Chinda Amadi <139082460+mansachi@users.noreply.github.com>
2024-01-09 04:55:53 +0100

    Delete 0x03-python-data-structures directory

--- 0x03-python-data-structures/0-print_list_integer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# 0x03-print_list_integer code
-
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i])) # end of code
--- 0x03-python-data-structures/2-replace_in_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 2-replace_in_list.py code
-
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    else:
-        my_list[idx] = element
-        return my_list # end of code
