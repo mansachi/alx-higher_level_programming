4563a5274d3bb183e616268535678474fe416d6d
mansachi <amadichinda1001@outlook.com>
2024-01-10 04:40:47 -0800

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 # code for 1-calculation.py
 
 if __name__ == "__main__":
-    """This prints the sum, the differenced, and multiple quotients of 10 and 5."""
+    """This prints the sum, the difference, and multiple quotients of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b))) # end of code
+    print("{} / {} = {}".format(a, b, div(a, b)))
+# end of code
